847877ad9b97194be11a65fdc9da42d65b692138
Erik Zachrisson <erik@zachrisson.info>
2014-04-18 14:01:29 +0200

    Add initial implementation

---
+++ source/SdramArbiter.vhd
@@ -0,0 +1,46 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+use work.Types.all;
+use work.DramTestPack.all;
+
+entity SdramArbiter is
+        port (
+        Clk         : in  bit1;
+        Rst_N       : in  bit1;
+        --
+        WriteReq    : in  DramRequest;
+        WriteReqAck : out bit1;
+        --
+        ReadReq     : in  DramRequest;
+        ReadReqAck  : out bit1;
+        --
+        ArbDecReq   : out DramRequest;
+        ArbDecVal   : out bit1
+        );
+end entity;
+
+architecture rtl of SdramArbiter is
+begin
+  ArbAsyn : process (ReadReq, WriteReq)
+  begin
+    WriteReqAck <= '0';
+    ReadReqAck <= '0';
+    ArbDecVal <= '0';
+    
+    if ReadReq.Val = "1" then
+      ReadReqAck <= '0';
+      ArbDecVal <= '1';
+      ArbDecReq <= ReadReq;
+    end if;
+
+    if WriteReq.Val = "1" then
+      WriteReqAck <= '1';
+      ReadReqAck  <= '0';
+      ArbDecVal   <= '1';
+      ArbDecReq   <= WriteReq;
+    end if;
+  end process;
+end architecture rtl;
