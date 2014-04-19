f8a10f9016e657fb5159210f922303c1e9e7e32f
Erik Zachrisson <erik@zachrisson.info>
2014-04-19 19:01:53 +0200

    Add response fifo

--- source/RespHandler.vhd
+++ source/RespHandler.vhd
@@ -8,9 +8,9 @@ use ieee.std_logic_unsigned.all;
 use work.Types.all;
 use work.DramTestPack.all;
 
-entity RespHdler is
+entity RespHandler is
   generic (
-    PixelW : positive := 9
+    PixelW : positive := 8
     );
   port (
     WrRst_N     : in  bit1;
@@ -31,10 +31,15 @@ entity RespHdler is
     );
 end entity;
 
-architecture rtl of RespHdler is
+architecture rtl of RespHandler is
   signal DataToVga : word(DSIZE-1 downto 0);
   signal FifoEmpty, ReadFifo, FifoFull : bit1;
   signal FillLvl : word(5-1 downto 0);
+
+  constant PixelsPerWord  : positive := DSIZE / PixelW;
+  constant PixelsPerWordW : positive := bits(PixelsPerWord);
+  
+  signal WordCnt_N, WordCnt_D : word(PixelsPerWordW-1 downto 0);
 begin
   
   RespFifo : entity work.RespFIFO
@@ -51,6 +56,38 @@ begin
       wrFull  => FifoFull
       );
 
-  assert not (WrReq = '1' and FifoFull = '1') report "Overflowing response fifo" severity failure;
+  assert not (RespDataVal = '1' and FifoFull = '1') report "Overflowing response fifo" severity failure;
+  SyncProc : process (RdClk, RdRst_N)
+  begin
+    if RdRst_N = '0' then
+      WordCnt_D <= (others => '0');
+    elsif rising_edge(RdClk) then
+      WordCnt_D <= WordCnt_N;
+    end if;
+  end process;
+
+  VgaFiller : process (WordCnt_D, DataToVga, FifoEmpty)
+  begin
+    -- Display black as default
+    PixelToDisp <= (others => '0');
+    WordCnt_N   <= WordCnt_D;
+    ReadFifo    <= '0';
+
+    -- Try to read out data when there is something to send
+    if FifoEmpty = '0' and WordCnt_D = 0 then
+      ReadFifo  <= '1';
+      WordCnt_N <= conv_word(1, WordCnt_N'length);
+    end if;
+
+    if InView = '1' then
+      PixelToDisp <= ExtractSlice(DataToVga, PixelW, conv_integer(WordCnt_D));
+
+      WordCnt_N <= WordCnt_D - 1;
+      if WordCnt_D = 0 then
+        WordCnt_N   <= (others => '0');
+        PixelToDisp <= (others => '0');
+      end if;
+    end if;
+  end process;
 
 end architecture rtl;
