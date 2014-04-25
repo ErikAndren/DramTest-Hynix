facb7e339934b9a2a0cef045458f9fd606f57584
Erik Zachrisson <erik@zachrisson.info>
2014-04-25 07:28:06 +0200

    Indentation. No functional change

--- source/DramTestPack.vhd
+++ source/DramTestPack.vhd
@@ -52,9 +52,9 @@ package DramTestPack is
   constant DRAM_LOAD_REG1     : word(CmdW-1 downto 0) := "110";
   constant DRAM_LOAD_REG2     : word(CmdW-1 downto 0) := "111";
 
-  constant tRCD       : positive := 3;
-  constant tCL        : positive := 2;
-  constant tRdDel     : positive := 2;
+  constant tRCD     : positive := 3;
+  constant tCL      : positive := 2;
+  constant tRdDel   : positive := 2;
   
   type DramRequest is record
     Val  : word1;
